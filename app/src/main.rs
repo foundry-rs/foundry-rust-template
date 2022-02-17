d162f44667c1bc2d9ef6281e7b585a3005da86df
Georgios Konstantopoulos <me@gakonst.com>
2022-02-17 23:25:54 +0200

    add rust ci

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,14 +1,16 @@
 use bindings::my_contract::MyContract;
 
-use ethers::{providers::Provider, prelude::Middleware, types::Address};
+use ethers::{prelude::Middleware, providers::Provider, types::Address};
 
+use eyre::Result;
 use std::convert::TryFrom;
 use std::sync::Arc;
-use eyre::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let provider = Provider::try_from("https://eth-rinkeby.alchemyapi.io/v2/Lc7oIGYeL_QvInzI0Wiu_pOZZDEKBrdf")?;
+    let provider = Provider::try_from(
+        "https://eth-rinkeby.alchemyapi.io/v2/Lc7oIGYeL_QvInzI0Wiu_pOZZDEKBrdf",
+    )?;
     let provider = Arc::new(provider);
 
     let address = "0x0000000000000000000000000000000000000000".parse::<Address>()?;
