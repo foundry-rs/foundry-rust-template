3f53523ae37db93f53b96ba2aed6383525b94c61
Matthias Seitz <matthias.seitz@outlook.de>
2022-12-01 20:14:18 +0100

    chore: rm Cargo.lock update bindings

--- bindings/src/my_contract.rs
+++ bindings/src/my_contract.rs
@@ -1,6 +1,6 @@
-pub use mycontract_mod::*;
-#[allow(clippy::too_many_arguments)]
-mod mycontract_mod {
+pub use my_contract::*;
+#[allow(clippy::too_many_arguments, non_camel_case_types)]
+pub mod my_contract {
     #![allow(clippy::enum_variant_names)]
     #![allow(dead_code)]
     #![allow(clippy::type_complexity)]
@@ -16,24 +16,37 @@ mod mycontract_mod {
     use ethers::providers::Middleware;
     #[doc = "MyContract was auto-generated with ethers-rs Abigen. More information at: https://github.com/gakonst/ethers-rs"]
     use std::sync::Arc;
+    # [rustfmt :: skip] const __ABI : & str = "[]" ;
+    #[doc = r" The parsed JSON-ABI of the contract."]
     pub static MYCONTRACT_ABI: ethers::contract::Lazy<ethers::core::abi::Abi> =
-        ethers::contract::Lazy::new(|| serde_json::from_str("[]").expect("invalid abi"));
-    #[derive(Clone)]
+        ethers::contract::Lazy::new(|| {
+            ethers::core::utils::__serde_json::from_str(__ABI).expect("invalid abi")
+        });
+    #[doc = r" Bytecode of the #name contract"]
+    pub static MYCONTRACT_BYTECODE: ethers::contract::Lazy<ethers::core::types::Bytes> =
+        ethers::contract::Lazy::new(|| {
+            "0x6080604052348015600f57600080fd5b50603f80601d6000396000f3fe6080604052600080fdfea26469706673582212201e72ce5cbbcc629d6ece181e9bab4aacf61266b450ffd6694df95221c6d33d0d64736f6c634300080a0033" . parse () . expect ("invalid bytecode")
+        });
     pub struct MyContract<M>(ethers::contract::Contract<M>);
+    impl<M> Clone for MyContract<M> {
+        fn clone(&self) -> Self {
+            MyContract(self.0.clone())
+        }
+    }
     impl<M> std::ops::Deref for MyContract<M> {
         type Target = ethers::contract::Contract<M>;
         fn deref(&self) -> &Self::Target {
             &self.0
         }
     }
-    impl<M: ethers::providers::Middleware> std::fmt::Debug for MyContract<M> {
+    impl<M> std::fmt::Debug for MyContract<M> {
         fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
             f.debug_tuple(stringify!(MyContract))
                 .field(&self.address())
                 .finish()
         }
     }
-    impl<'a, M: ethers::providers::Middleware> MyContract<M> {
+    impl<M: ethers::providers::Middleware> MyContract<M> {
         #[doc = r" Creates a new contract instance with the specified `ethers`"]
         #[doc = r" client at the given `Address`. The contract derefs to a `ethers::Contract`"]
         #[doc = r" object"]
@@ -41,8 +54,50 @@ mod mycontract_mod {
             address: T,
             client: ::std::sync::Arc<M>,
         ) -> Self {
-            let contract =
-                ethers::contract::Contract::new(address.into(), MYCONTRACT_ABI.clone(), client);
+            ethers::contract::Contract::new(address.into(), MYCONTRACT_ABI.clone(), client).into()
+        }
+        #[doc = r" Constructs the general purpose `Deployer` instance based on the provided constructor arguments and sends it."]
+        #[doc = r" Returns a new instance of a deployer that returns an instance of this contract after sending the transaction"]
+        #[doc = r""]
+        #[doc = r" Notes:"]
+        #[doc = r" 1. If there are no constructor arguments, you should pass `()` as the argument."]
+        #[doc = r" 1. The default poll duration is 7 seconds."]
+        #[doc = r" 1. The default number of confirmations is 1 block."]
+        #[doc = r""]
+        #[doc = r""]
+        #[doc = r" # Example"]
+        #[doc = r""]
+        #[doc = r" Generate contract bindings with `abigen!` and deploy a new contract instance."]
+        #[doc = r""]
+        #[doc = r" *Note*: this requires a `bytecode` and `abi` object in the `greeter.json` artifact."]
+        #[doc = r""]
+        #[doc = r" ```ignore"]
+        #[doc = r" # async fn deploy<M: ethers::providers::Middleware>(client: ::std::sync::Arc<M>) {"]
+        #[doc = r#"     abigen!(Greeter,"../greeter.json");"#]
+        #[doc = r""]
+        #[doc = r#"    let greeter_contract = Greeter::deploy(client, "Hello world!".to_string()).unwrap().send().await.unwrap();"#]
+        #[doc = r"    let msg = greeter_contract.greet().call().await.unwrap();"]
+        #[doc = r" # }"]
+        #[doc = r" ```"]
+        pub fn deploy<T: ethers::core::abi::Tokenize>(
+            client: ::std::sync::Arc<M>,
+            constructor_args: T,
+        ) -> ::std::result::Result<
+            ethers::contract::builders::ContractDeployer<M, Self>,
+            ethers::contract::ContractError<M>,
+        > {
+            let factory = ethers::contract::ContractFactory::new(
+                MYCONTRACT_ABI.clone(),
+                MYCONTRACT_BYTECODE.clone().into(),
+                client,
+            );
+            let deployer = factory.deploy(constructor_args)?;
+            let deployer = ethers::contract::ContractDeployer::new(deployer);
+            Ok(deployer)
+        }
+    }
+    impl<M: ethers::providers::Middleware> From<ethers::contract::Contract<M>> for MyContract<M> {
+        fn from(contract: ethers::contract::Contract<M>) -> Self {
             Self(contract)
         }
     }
